fa29910225c80d936de996d3b65e6f9e104a9b01
kirazero17 <84077194+kirazero17@users.noreply.github.com>
2026-04-02 16:32:23 +0700

    init 4g handler

--- Sim_hander.h
+++ Sim_hander.h
@@ -12,6 +12,79 @@
 #define DUMP_AT_COMMANDS
 #define TINY_GSM_DEBUG SerialMon
 
+// pins
+#define MODEM_RX 17
+#define MODEM_TX 16
+// #define MODEM_RESET_PIN 4
+
 #include <TinyGsmClient.h>
+
+#ifdef DUMP_AT_COMMANDS
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, SerialMon);
+TinyGsm        modem(debugger);
+#else
+TinyGsm        modem(SerialAT);
+#endif
+
+TinyGsmClient client(modem);
+
+const char apn[]      = "v-internet";  // Thay bằng APN của nhà mạng bạn
+const char gprsUser[] = "";
+const char gprsPass[] = "";
+
+void setupGSM() {
+    int retrys = 1;
+    SerialMon.println("Initializing modem... Attempt: " + String(retrys));
+    modem.restart();
+    String modemInfo = modem.getModemInfo();
+    while (modemInfo.length() == 0) {
+        if (retrys > 5) {
+            SerialMon.println("Failed to initialize modem after multiple attempts");
+            return;
+        }
+        SerialMon.println("Failed to get modem info, retrying... Attempt: " 
+            + String(retrys));
+        delay(1000);
+        modem.restart();
+        modemInfo = modem.getModemInfo();
+        retrys++;
+    }
+    SerialMon.print("Modem Info: ");
+    SerialMon.println(modemInfo);
+
+    SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+    delay(3000);
+    
+    SerialMon.println("Connecting to GSM network...");
+    if (!modem.waitForNetwork()) {
+        SerialMon.println("Failed to connect to GSM network");
+        return;
+    }
+    
+    if (modem.isNetworkConnected()) {
+        SerialMon.println("GSM network connected");
+    } else {
+        SerialMon.println("GSM network connection failed");
+        return;
+    }
+
+    int csq = modem.getSignalQuality();
+    SerialMon.print("Signal quality: " + String(csq));
+
+    SerialMon.print("Connecting to APN: ");
+    SerialMon.print(apn);
+    if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+            SerialMon.println(" - Fail !");
+            delay(10000);
+            return;
+    }
+    SerialMon.println(" - Success !");
+    if (modem.isGprsConnected()) {
+        SerialMon.println("GPRS connected");
+    }
+    IPAddress local = modem.localIP();
+    SerialMon.println("Local IP: " + local.toString());
+}
 
 #endif
